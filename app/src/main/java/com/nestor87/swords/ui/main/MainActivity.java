6ecec919d64c88c522cc1bf20474b34eeff0177b
Nestorian <ananiev.nestor@ukr.net>
2021-10-11 17:33:06 +0300

    Fix version dialog #3

--- app/src/main/java/com/nestor87/swords/ui/main/MainActivity.java
+++ app/src/main/java/com/nestor87/swords/ui/main/MainActivity.java
@@ -113,9 +113,6 @@ public class MainActivity extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         setContentView(R.layout.activity_main);
 
-        requestPermission();
-
-
         SharedPreferences preferences = getSharedPreferences(APP_PREFERENCES_FILE_NAME, MODE_PRIVATE);
         SharedPreferences.Editor preferencesEditor = preferences.edit();
         preferencesEditor.putString("accountId", preferences.getString("accountId", UUID.randomUUID().toString()));
@@ -123,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
 
         uuid = preferences.getString("accountId", "");
 
-
+        requestPermission();
 
         scoreTextView = findViewById(R.id.score);
         hintsTextView = findViewById(R.id.hints);
